5a806df3239a857e5e56d5786cc945a25ecb8247
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-02-25 14:52:23 -0500

    chkpt ClientRunner

--- akd/src/tests/test_pav_serv.rs
+++ akd/src/tests/test_pav_serv.rs
@@ -1,9 +1,9 @@
-use std::sync::Arc;
-use std::time::Instant;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
 
 use akd_core::ecvrf::HardCodedAkdVRF as VRF;
 use akd_core::verify::history::HistoryParams;
-use akd_core::verify::lookup_verify;
+use akd_core::verify::{key_history_verify, lookup_verify, HistoryVerificationParams};
 use akd_core::{AkdLabel, AkdValue};
 use rand::{rngs::StdRng, SeedableRng};
 use rand::{CryptoRng, Rng};
@@ -98,7 +98,9 @@ async fn put_batch_helper(batch_size: i32) {
                     .await
             });
         }
-        while let Some(_) = join_set.join_next().await {}
+        while let Some(res) = join_set.join_next().await {
+            let _ = res.unwrap();
+        }
     }
     let total = start.elapsed();
 
@@ -126,14 +128,62 @@ async fn put_batch_helper(batch_size: i32) {
 }
 
 #[tokio::test(flavor = "multi_thread")]
+async fn bench_serv_put_verify() {
+    let (mut _rng, dir, labels, _) = seed_server(DEF_NSEED).await;
+    let vrf_pk = dir.get_public_key().await.unwrap();
+    let n_ops = 2_000;
+    let n_warm = get_warmup(n_ops);
+
+    let mut total: Duration = Default::default();
+    for i in 0..n_warm + n_ops {
+        if i == n_warm {
+            total = Default::default();
+        }
+        let l = &labels[i as usize % DEF_NSEED];
+        let (p, dig) = dir
+            .key_history(l, HistoryParams::MostRecent(1))
+            .await
+            .unwrap();
+
+        let s = Instant::now();
+        key_history_verify::<TC>(
+            vrf_pk.as_bytes(),
+            dig.hash(),
+            dig.epoch(),
+            l.clone(),
+            p,
+            HistoryVerificationParams::default(),
+        )
+        .unwrap();
+        total += s.elapsed();
+    }
+
+    let m0 = total.as_micros() as f64 / n_ops as f64;
+    let m1 = total.as_millis() as f64;
+    report(
+        "bench_serv_put_verify".into(),
+        n_ops,
+        &[
+            &Metric {
+                n: m0,
+                unit: "us/op".into(),
+            },
+            &Metric {
+                n: m1,
+                unit: "total(ms)".into(),
+            },
+        ],
+    );
+}
+
+#[tokio::test(flavor = "multi_thread")]
 async fn bench_serv_put_size() {
     let (mut rng, dir, _labels, _) = seed_server(DEF_NSEED).await;
-    let elems: Vec<(AkdLabel, AkdValue)> = (0..1)
-        .map(|_| (mk_rand_label(&mut rng), mk_def_val()))
-        .collect();
-    dir.publish(elems.clone()).await.unwrap();
+    let l = mk_rand_label(&mut rng);
+    let elem = vec![(l.clone(), mk_def_val())];
+    dir.publish(elem).await.unwrap();
     let (p, dig) = dir
-        .key_history(&elems[0].0, HistoryParams::MostRecent(1))
+        .key_history(&l, HistoryParams::MostRecent(1))
         .await
         .unwrap();
     let pb = bincode::serialize(&p).unwrap();
@@ -150,25 +200,25 @@ async fn bench_serv_put_size() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn bench_serv_get() {
+async fn bench_serv_get_one() {
     let (mut _rng, dir, labels, _) = seed_server(DEF_NSEED).await;
-    let vrf_pk = dir.get_public_key().await.unwrap();
-    let n_ops: i32 = 3_000;
+    let n_ops = 3_000;
+    let n_warm = get_warmup(n_ops);
 
-    let start = Instant::now();
-    for i in 0..n_ops {
+    let mut start = Instant::now();
+    for i in 0..n_warm + n_ops {
+        if i == n_warm {
+            start = Instant::now();
+        }
         let l = &labels[i as usize % DEF_NSEED];
-        let (p, dig) = dir.lookup(l.clone()).await.unwrap();
-        // note: each key only has one version.
-        // note: run sequentially.
-        lookup_verify::<TC>(vrf_pk.as_bytes(), dig.hash(), dig.epoch(), l.clone(), p).unwrap();
+        dir.lookup(l.clone()).await.unwrap();
     }
     let total = start.elapsed();
 
     let m0 = total.as_micros() as f64 / n_ops as f64;
     let m1 = total.as_millis() as f64;
     report(
-        "bench_serv_get".into(),
+        "bench_serv_get_one".into(),
         n_ops,
         &[
             &Metric {
@@ -180,6 +230,28 @@ async fn bench_serv_get() {
                 unit: "total(ms)".into(),
             },
         ],
+    );
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn bench_serv_get_size_one() {
+    let (mut _rng, dir, labels, _) = seed_server(DEF_NSEED).await;
+    let l = &labels[0];
+    dir.lookup(l.clone()).await.unwrap();
+    let (p, dig) = dir
+        .key_history(l, HistoryParams::MostRecent(1))
+        .await
+        .unwrap();
+    let pb = bincode::serialize(&p).unwrap();
+    let digb = bincode::serialize(&dig).unwrap();
+    let sz = (pb.len() + digb.len()) as f64;
+    report(
+        "bench_serv_put_size".into(),
+        1,
+        &[&Metric {
+            n: sz,
+            unit: "B".into(),
+        }],
     );
 }
 
@@ -279,4 +351,124 @@ fn mk_def_val() -> AkdValue {
 
 fn get_warmup(n_ops: i32) -> i32 {
     return (n_ops as f64 * 0.1) as i32;
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn bench_serv_get_scale() {
+    let (mut _rng, dir, labels, _) = seed_server(DEF_NSEED).await;
+    let arc_labels = Arc::new(labels);
+    // TODO: change.
+    let max_n_cli = 10;
+    let mut runner = ClientRunner::new(max_n_cli);
+
+    for n_cli in 1..=max_n_cli {
+        let dir_clone = dir.clone();
+        let labels_clone = arc_labels.clone();
+        let total_time = runner.run(n_cli, async move || {
+            // TODO: figure out rand in tokio.
+            let l = &labels_clone[0];
+            dir_clone.lookup(l.clone()).await.unwrap();
+        });
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct StartEnd {
+    start: Instant,
+    end: Instant,
+}
+
+struct Sample {
+    xs: Vec<f64>,
+}
+
+struct ClientRunner {
+    times: Vec<Arc<Mutex<Vec<StartEnd>>>>,
+    sample: Sample,
+}
+
+impl ClientRunner {
+    fn new(max_n_cli: usize) -> Self {
+        let mut times = Vec::with_capacity(max_n_cli);
+        for _ in 0..max_n_cli {
+            times.push(Arc::new(Mutex::new(Vec::with_capacity(1_000_000))));
+        }
+        let sample = Sample {
+            xs: Vec::with_capacity(10_000_000),
+        };
+        ClientRunner { times, sample }
+    }
+
+    async fn run<F, Fut>(&mut self, n_cli: usize, work: F) -> Duration
+    where
+        F: Fn() -> Fut + Send + Sync + 'static + Clone,
+        Fut: std::future::Future<Output = ()> + Send + 'static,
+    {
+        // Clear previous results
+        for i in 0..n_cli {
+            self.times[i].lock().unwrap().clear();
+        }
+
+        // Get data.
+        let mut join_handles = Vec::with_capacity(n_cli);
+        for i in 0..n_cli {
+            let times = Arc::clone(&self.times[i]);
+            let work_clone = work.clone();
+
+            let handle = tokio::spawn(async move {
+                let begin = Instant::now();
+                loop {
+                    let start = Instant::now();
+                    work_clone().await;
+                    let end = Instant::now();
+                    times.lock().unwrap().push(StartEnd { start, end });
+
+                    if end - begin >= Duration::from_secs(1) {
+                        break;
+                    }
+                }
+            });
+            join_handles.push(handle);
+        }
+        for handle in join_handles {
+            handle.await.unwrap();
+        }
+
+        // Calculate time bounds
+        let mut starts = Vec::with_capacity(n_cli);
+        let mut ends = Vec::with_capacity(n_cli);
+        for i in 0..n_cli {
+            let times = self.times[i].lock().unwrap();
+            if let Some(first) = times.first() {
+                starts.push(first.start);
+            }
+            if let Some(last) = times.last() {
+                ends.push(last.end);
+            }
+        }
+
+        let init = starts.into_iter().max().unwrap();
+        let post_warm = init + Duration::from_millis(100);
+        let end = ends.into_iter().min().unwrap();
+        let total = end - post_warm;
+
+        // Collect samples
+        self.sample.xs.clear();
+        for i in 0..n_cli {
+            let times = self.times[i].lock().unwrap();
+            let low = times.partition_point(|s| s.start < post_warm);
+            let high = times.partition_point(|s| s.end <= end);
+            if (high as i64) - (low as i64) < 1_000 {
+                panic!("ClientRunner: clients don't have enough overlapping samples");
+            }
+
+            for j in low..high {
+                let d = (times[j].end - times[j].start).as_nanos() as f64;
+                self.sample.xs.push(d);
+            }
+        }
+
+        self.sample.xs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        total
+    }
 }
