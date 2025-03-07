81c04be47793a0f1a80231090d9da6bf05d840d7
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-03-07 09:39:34 -0500

    better err support in run.py

--- etc/run.py
+++ etc/run.py
@@ -7,28 +7,32 @@ samply = False
 if len(sys.argv) != 2:
     samply = True
 
+build_cmd = [
+    "cargo",
+    "test",
+    "--profile",
+    "benchtest",
+    "--features",
+    "benchtest",
+    "--no-default-features",
+    "--package",
+    "akd",
+    "--no-run",
+    "--message-format",
+    "json",
+]
 p = sp.run(
-    [
-        "cargo",
-        "test",
-        "--profile",
-        "benchtest",
-        "--features",
-        "benchtest",
-        "--no-default-features",
-        "--package",
-        "akd",
-        "--message-format",
-        "json",
-        "--no-run",
-    ],
+    build_cmd,
     text=True,
     capture_output=True,
 )
 akd_build = json.loads(p.stdout.splitlines()[-2])
+if "executable" not in akd_build:
+    print("error: failed build cmd:", " ".join(build_cmd[:-2]))
+    sys.exit(1)
+
 exe = akd_build["executable"]
 test_cmd = [exe, "--nocapture", "--test-threads", "1", sys.argv[1]]
-
 if samply:
     samply_cmd = [
         "samply",
